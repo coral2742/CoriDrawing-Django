affd8ad1f7b039f5c49ac04f5e568996db251cb9
coral.izquierdo <coral.izquierdo@satec.es>
2022-08-18 14:10:18 +0200

    Heroku

--- coridrawing/settings.py
+++ coridrawing/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,6 +86,8 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+DATABASES["default"].update(dj_database_url.config(conn_max_age=600))
 
 
 # Password validation
--- coridrawing/urls.py
+++ coridrawing/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path("", include("drawings.urls")),
     path("admin/", admin.site.urls),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
