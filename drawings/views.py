23d6d16bba65d2ed492e7dd4fc20e525237a5b8d
coral <coral2742@gmail.com>
2023-04-07 15:15:28 +0200

    ☁️ AWS S3 bucket - añadidas imágenes

--- drawings/views.py
+++ drawings/views.py
@@ -12,7 +12,7 @@ class Index(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["categories"] = DrawingCategory.objects.all()
+        context["categories"] = DrawingCategory.objects.all().order_by('priority')
         return context
 
 
