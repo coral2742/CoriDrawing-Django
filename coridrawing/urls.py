736f22692206558cb40aca93dc75229af5591182
coral <coral2742@gmail.com>
2023-05-02 21:01:33 +0200

    :rocket: RDS para BBDD implementado

--- coridrawing/urls.py
+++ coridrawing/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path("", include("drawings.urls")),
