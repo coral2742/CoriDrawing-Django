169336ad0e1bd3d3fd29db0e592764de371f802b
coral <coral2742@gmail.com>
2023-05-02 19:22:18 +0200

    :rocket: RDS para BBDD implementado

--- coridrawing/settings.py
+++ coridrawing/settings.py
@@ -31,12 +31,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9@&4apqyb_oq+srxc83an3y2ge+-qyk!kdgk(=qysorx(^%ts&"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS=['*', 'CoriDrawing.eu-north-1.elasticbeanstalk.com']
+ALLOWED_HOSTS=['CoriDrawing.eu-north-1.elasticbeanstalk.com']
 
 
 # Application definition
