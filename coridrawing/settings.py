b7ae5995358d495a3026e9d1bcbe0901470106c6
coral <coral2742@gmail.com>
2023-05-02 21:48:23 +0200

    :rocket: RDS para BBDD implementado

--- coridrawing/settings.py
+++ coridrawing/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS=['CoriDrawing.eu-north-1.elasticbeanstalk.com', '127.0.0.1']
 
