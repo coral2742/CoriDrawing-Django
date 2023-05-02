d8763234900704e5b627d0f2e735eebd1c6f6c5f
coral <coral2742@gmail.com>
2023-05-02 18:49:08 +0200

    :rocket: RDS para BBDD implementado

--- coridrawing/settings.py
+++ coridrawing/settings.py
@@ -86,38 +86,14 @@ WSGI_APPLICATION = "coridrawing.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nhcrzkjj',
-        'USER': 'nhcrzkjj',
-        'PASSWORD': 'KVeDY8gc2ZoTcCKTul8CKiGUgVOmwPh7',
-        'HOST': 'mel.db.elephantsql.com',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('RDS_DB_NAME'),
+        'USER': env('RDS_USERNAME'),
+        'PASSWORD': env('RDS_PASSWORD'),
+        'HOST': env('RDS_HOSTNAME'),
+        'PORT': env('RDS_PORT'),
     }
 }
-
-
-# # if 'RDS_DB_NAME' in env:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': env['RDS_DB_NAME'],
-#             'USER': env['RDS_USERNAME'],
-#             'PASSWORD': env['RDS_PASSWORD'],
-#             'HOST': env['RDS_HOSTNAME'],
-#             'PORT': env['RDS_PORT'],
-#         }
-#     }
-# # else:
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'nhcrzkjj',
-#         'USER': 'nhcrzkjj',
-#         'PASSWORD': 'KVeDY8gc2ZoTcCKTul8CKiGUgVOmwPh7',
-#         'HOST': 'mel.db.elephantsql.com',
-#         'PORT': '5432'
-#     }
-# }
 
 
 # Password validation
